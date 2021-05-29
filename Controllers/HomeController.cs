94bcf8e8a1cba7b7313ce35e91278e31442cdc44
wandercode <syamilsasi4s@gmail.com>
2021-05-29 18:34:31 +0530

    hirebrainchild content page added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace SimpleApp.Controllers
             return View();
         }
 
-        public IActionResult Hire()
+        public IActionResult HireBrainChild()
         {
             return View();
         }
